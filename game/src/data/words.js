224df06a3d1d927358961b07ac716ff4a69f293f
Larissa Araujo <lari_araujo093@hotmail.com>
2024-03-07 07:19:06 -0300

    finish

---
+++ game/src/data/words.js
@@ -0,0 +1,8 @@
+export const wordsList = {
+    carro: ['Motor', 'Porta', 'Lanterna', 'Farol', 'Banco'],
+    fruta: ['Laranja', 'Pera', 'Abacaxi', 'Banana', 'Acerola'],
+    corpo: ['Braço', 'Perna', 'Cabeça', 'Bochecha', 'Barriga'],
+    computador: ['Mouse', 'Teclado', 'Monitor', 'Camera'],
+    programação: ['Linguagem', 'Framework', 'JavaScript', 'React'],
+    alimento: ['Feijao', 'Macarrao', 'Carne', 'Salada'],
+};
